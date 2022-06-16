b986ddebf9ebcd3bdc30805f503c66f7ed4906d9
Karam <krm-011@hotmail.com>
2022-06-16 21:44:11 +0200

    fix 1

--- ardemiumAppTemplate/src/main/java/backend/Admin.java
+++ ardemiumAppTemplate/src/main/java/backend/Admin.java
@@ -1,8 +1,32 @@
 package backend;
 
-public class Admin extends User {
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
 
-    public Admin(User u) {
+public class Admin extends User {
+    public ArrayList<User>employees;
+    public ArrayList<Quote>quotes;
+    public ArrayList<Reward>allRewards;
+    public DatabaseConnection dc = new DatabaseConnection();
+    public Admin(User u) throws FileNotFoundException {
         super(u.USERID, u.email, u.password, u.name, u.phone);
+        this.employees = initEmployees();
+        this.quotes = initQuotes();
+        this.allRewards = initAllRewards();
     }
+
+    public ArrayList<User> initEmployees() throws FileNotFoundException {
+        return dc.getUserdb();
+    }
+
+    public ArrayList<Reward> initAllRewards() throws FileNotFoundException {
+        return dc.getRewarddb();
+
+    }
+
+    public ArrayList<Quote> initQuotes() throws FileNotFoundException {
+        return dc.getQuotesdb();
+    }
+
+
 }
